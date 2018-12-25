147f805b8c2e007cc57325421e7e52643ac7e213
Giorgio Delgado <hi@gdelgado.ca>
2018-12-25 10:52:52 -0500

    Use helmetjs

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ require('dotenv').config()
 
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
+import * as helmet from 'helmet'
+
 import routes from './routes'
 
 const app = express()
 const port = 3000
 
+app.use(helmet())
 app.use(bodyParser.json())
 
 app.use(routes)
