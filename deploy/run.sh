8f9a537df44627b62e87a89e9c8f01fc62dabf83
Giorgio Delgado <hi@gdelgado.ca>
2018-12-30 20:27:01 -0500

    Add CI deploy script

---
+++ deploy/run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Exit script if you try to use an uninitialized variable.
+set -o nounset
+
+# Exit script if a statement returns a non-true return value.
+# set -o errexit
+
+# Use the error status of the first failure, rather than that of the last item in a pipeline.
+set -o pipefail
+
+COMMIT_REF="$1"
+
+git fetch --all
+git -c advice.detachedHead=false checkout -f "origin/$COMMIT_REF"
+
+export COMPOSE_FILE="docker-compose.deploy.yml"
+
+# pull images and run service
+docker-compose up -d
