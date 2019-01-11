7ff3af4e6417d4007e387f98d833d970dd8c5b9e
Giorgio Delgado <hi@gdelgado.ca>
2019-01-11 17:30:26 -0500

    Pass env vars to ssh

--- deploy/run.sh
+++ deploy/run.sh
@@ -14,14 +14,18 @@ COMMIT_REF="$1"
 echo "booting up docker for branch:"
 echo $COMMIT_REF
 
+rm -rf .env
+
+echo "PG_USERNAME=$2" >> .env
+echo "PG_PASS=$3" >> .env
+echo "GIT_SHA1=$4" >> .env
+
 git fetch --all
 git -c advice.detachedHead=false checkout -f "origin/$COMMIT_REF"
 
-export PG_USERNAME="$PG_USERNAME"
-export PG_PASS="$PG_PASS"
-export CIRCLE_SHA1="$CIRCLE_SHA1"
-
 export COMPOSE_FILE="docker-compose.deploy.yml"
+
+source .env
 
 # pull images and run service
 docker-compose up -d
