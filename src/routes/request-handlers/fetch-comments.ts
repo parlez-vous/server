deadbd93d4ab0dc0f81d8d1410565e3d743f59ec
Giorgio Delgado <hi@gdelgado.ca>
2019-04-06 20:22:08 -0400

    Remove unused route

--- src/routes/request-handlers/fetch-comments.ts
+++
@@ -1,6 +0,0 @@
-import { fetchComments } from 'db/actions'
-import { route, getMeta } from './middleware'
-
-export const handler = route((req) =>
-  getMeta(req).mapOk(fetchComments)
-)
--- src/routes/request-handlers/index.ts
+++ src/routes/request-handlers/index.ts
@@ -1,4 +1,3 @@
-export { handler as fetchComments } from './fetch-comments'
 export { handler as addComment } from './add-comment'
 export { handler as adminSignup } from './admin-signup'
 export { handler as adminSignin } from './admin-signin'
