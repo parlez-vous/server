c170196c37eac88080052aecb4b05cf0730c0361
Giorgio Delgado <hi@gdelgado.ca>
2020-03-12 17:06:47 -0400

    Update app yaml generator config

--- generate-app-yaml.py
+++ generate-app-yaml.py
@@ -20,20 +20,9 @@ app_yaml_base = """
 # convert raw yaml string into a dict
 obj = yaml.load(app_yaml_base)
 
-
-cloud_sql_instances = os.environ['CLOUD_SQL_INSTANCES']
-
 # dynamically add sensitive environment variables
 obj['env_variables'].update({
-  "DATABASE_HOST": "/cloudsql/" + cloud_sql_instances,
-  "DATABASE_USER": os.environ['DATABASE_USER'],
-  "DATABASE_PASSWORD": os.environ['DATABASE_PASSWORD'],
-  "DATABASE_NAME": os.environ['DATABASE_NAME'],
   "CRON_INTERVAL_MS": os.environ['CRON_INTERVAL_MS']
 })
-
-obj['beta_settings'] = {
-  "cloud_sql_instances": cloud_sql_instances
-}
 
 print(yaml.dump(obj, default_flow_style=False))
