49286d07f5bf31b0e7db8da926a77a6dcf35bd09
Giorgio Delgado <hi@gdelgado.ca>
2020-03-12 23:41:07 -0400

    Log string in json format

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,10 @@ app.listen(
 startCronJobs()
 
 process.on('unhandledRejection', (reason, promise) => {
-  console.log('Unhandled Rejection at:', promise, 'reason:', reason)
+  console.log({
+    error: `Unhandled Rejection at: ${promise},\nReason: ${reason}`
+  })
+
   // Application specific logging, throwing an error, or other logic here
 
   process.exit(1)
