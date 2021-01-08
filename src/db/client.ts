3ca80e9c121caf7bd33d36492a640eeb8fb874f8
Giorgio Delgado <hi@gdelgado.ca>
2021-01-08 13:09:44 -0500

    Log slow queries to sdout

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,4 +1,20 @@
 import { PrismaClient } from '@prisma/client'
+import logger from 'logger'
 
 // expose a singleton
-export const client = new PrismaClient()
+export const client = new PrismaClient({
+  log: [
+    {
+      emit: 'event',
+      level: 'query',
+    }
+  ],
+})
+
+// https://www.prisma.io/docs/concepts/components/prisma-client/logging#logging-query-events
+client.$on('query', e => {
+  if (e.duration > 100) {
+    logger.warn(`Slow query (${e.duration}ms) - Query:\n${e.query}`)
+  }
+})
+
