98cf7d3e55d0bc9b1fad960f82faeb54288749a8
asrar <aszenz@gmail.com>
2021-05-04 17:06:23 +0530

    admin endpoint to get latest site comments

--- src/routes/admins/sites/index.ts
+++ src/routes/admins/sites/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express'
 import { handler as getAllSites } from './get-all'
 import { handler as getSingle } from './get-single'
 import { handler as registerSite } from './register-site'
+import { handler as getSingleSiteComments } from './get-site-comments'
 
 const router = Router()
 
 router.get('/', getAllSites)
 router.post('/register', registerSite)
 router.get('/:id', getSingle)
+router.get('/:id/comments', getSingleSiteComments)
 
 export default router
